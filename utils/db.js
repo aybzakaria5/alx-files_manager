e4e282a338e39da984945cd88d2d8d001ce441e4
Njine <brikab@gmail.com>
2024-06-15 21:24:08 +0000

    create URL connection

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,16 @@
 // Import MongoClient from mongodb
 
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
+
+// Define a function to connect to the database
+class DBClient {
+    constructor() {
+        // Get environment variables
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+
+        // Create the URL connection
+        const url = `mongodb://${host}:${port}`;
+
+}
