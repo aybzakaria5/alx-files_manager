9dadadb317d797b330726b45819e6847b67d2473
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-06-15 16:31:48 +0100

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# alx-files_manager
+## files manager project
+
+
+This project is a summary of this back-end trimester: authentication, NodeJS, MongoDB, Redis, pagination and background processing.
+
+The objective is to build a simple platform to upload and view files:
+
+    User authentication via a token
+    List all files
+    Upload a new file
+    Change permission of a file
+    View a file
+    Generate thumbnails for images
+
+You will be guided step by step for building it, but you have some freedoms of implementation, split in more files etc… (utils folder will be your friend)
+
+Of course, this kind of service already exists in the real life - it’s a learning purpose to assemble each piece and build a full product.
+
+Enjoy!
---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error}`);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
