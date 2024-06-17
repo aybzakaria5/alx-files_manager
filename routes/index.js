c2a4bbb77bce6a1940746652e9b8f98237704603
Njine <brikab@gmail.com>
2024-06-17 02:31:32 +0000

    added the routes

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+// Import { Router } from 'express' and controllers
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+
+// Create a new router
+const router = Router();
+
+// Create a new route for the root path
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+// Export router
+export default router;
