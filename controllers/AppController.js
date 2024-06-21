99c854b54e3b1e8b585f478a1886d9678b63ad2f
Njine <brikab@gmail.com>
2024-06-21 20:33:18 +0000

    dark shot

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,17 @@ const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
-  static async getStatus(request, response) {
-    response.status(200).json({
+  static async getStatus(req, res) {
+    res.status(200).json({
       redis: redisClient.isAlive(),
       db: await dbClient.isAlive(),
-});
+    });
   }
 
-  static async getStats(request, response) {
+  static async getStats(req, res) {
     const usersNum = await dbClient.nbUsers();
     const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+    res.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
+//import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -12,8 +12,8 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+//router.post('/files', FilesController.postUpload);
+//router.get('/files/:id', FilesController.getShow);
+//router.get('/files', FilesController.getIndex);
 
 export default router;
